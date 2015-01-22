019de2a1d7ba294afa6a5969f884a33a7dfb2ade
MaxMEllon <mozi_kke@excite.co.jp>
2015-01-23 05:26:12 +0900

    edit: animation実装

--- build/script/app.js
+++ build/script/app.js
@@ -1,6 +1,29 @@
 (function() {
-  if ($.support.pjax) {
-    $(document).pjax('a[data-pjax]', '#pjax-container');
-  }
+  $(document).ready(function() {
+    return $('.main').pjax({
+      link: 'a.pjax',
+      area: '#pjax-container',
+      callback: function() {
+        return $('#pjax-container').css({
+          left: 20
+        }).animate({
+          left: 0,
+          opacity: 1
+        }, 100);
+      },
+      callbacks: {
+        before: function() {
+          return $('#pjax-container').animate({
+            left: -20,
+            opacity: 0
+          }, 100);
+        }
+      },
+      ajax: {
+        timeout: 3000
+      },
+      wait: 120
+    });
+  });
 
 }).call(this);
--- src/coffee/app.coffee
+++ src/coffee/app.coffee
@@ -1,3 +1,23 @@
-if $.support.pjax
-  $(document).pjax('a[data-pjax]', '#pjax-container')
-
+# pjax
+$(document).ready ->
+  $('.main').pjax
+    link: 'a.pjax'
+    area: '#pjax-container'
+    callback:
+      ->
+        $('#pjax-container').css(
+          left: 20
+        ).animate(
+          left: 0,
+          opacity: 1
+        , 100)
+    callbacks:
+      before:
+        ->
+          $('#pjax-container').animate(
+            left: -20,
+            opacity: 0
+          , 100)
+    ajax:
+      timeout: 3000
+    wait: 120
